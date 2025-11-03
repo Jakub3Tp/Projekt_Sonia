f84b25daf123c6e7a770f0eaabd45135efa19842
kubix <jakub.gola2@teb.edu.pl>
2025-11-03 18:36:33 +0100

    03.11.2025 - Layout merging

--- app/src/main/java/com/example/sonia/HomeActivity.java
+++ app/src/main/java/com/example/sonia/HomeActivity.java
@@ -3,15 +3,24 @@ package com.example.sonia;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            return insets;
+        });
     }
-
     public void openRoom(View view){
         Intent intent = new Intent(this, RoomActivity.class);
 
@@ -22,6 +31,8 @@ public class HomeActivity extends AppCompatActivity {
             intent.putExtra("roomName", "Living Room");
         }else if (id == R.id.kitchenCard) {
             intent.putExtra("roomName", "Kitchen");
+        } else if (id == R.id.bathroomCard) {
+            intent.putExtra("roomName", "Bathroom");
         }
 
         startActivity(intent);
