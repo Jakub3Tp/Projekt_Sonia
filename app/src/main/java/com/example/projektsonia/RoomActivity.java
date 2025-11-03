f3045e8bece0b4d4e384c283ce4c45cfcc0d2c1f
5TpNowickaMonika <egzamin@poczta.pl>
2025-11-03 13:25:27 +0100

    03.11.2025 - Widocznosc

--- app/src/main/java/com/example/projektsonia/RoomActivity.java
+++ app/src/main/java/com/example/projektsonia/RoomActivity.java
@@ -16,6 +16,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class RoomActivity extends AppCompatActivity {
 
     @Override
